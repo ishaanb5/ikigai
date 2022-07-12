de43a60fcf4714e14da6d2af2db441de62e08811
Ishaan Bhandari <17184774+ishaanb5@users.noreply.github.com>
2022-07-12 23:04:23 +0530

    fix: error message for duplicate error

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -9,7 +9,7 @@ const errorHandler = (err, req, res, next) => {
 
   if (err.name === 'CastError') {
     const taskDoesNotExist = new RegExp(
-      /created to have an id of a task that does not exist in db/
+      /created to have an id of a task that does not exist in db/,
     )
     if (taskDoesNotExist.test(err.value))
       return res.status(404).json({ error: 'not found' })
@@ -23,7 +23,7 @@ const errorHandler = (err, req, res, next) => {
   }
 
   if (err.name === 'MongoServerError' && err.code === 11000) {
-    return res.status(400).json({ error: 'err.message' })
+    return res.status(400).json({ error: err.message })
   }
 
   return res.status(400).json({ error: err.message })
