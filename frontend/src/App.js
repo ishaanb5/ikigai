1bb5cb7313499cd7984f4043d3a345b18f21d544
Ishaan Bhandari <17184774+ishaanb5@users.noreply.github.com>
2022-07-22 23:43:08 +0530

    refactor: tasklist receives task as props from App component
    state is being lifted so that tasks can be changed when a different list is selected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import TaskList from './TaskList'
 import CategoryList from './CategoryList'
+import taskService from './services/tasks'
 
 import './index.css'
 
 const App = () => {
-  const [currentList, setCurrentList] = useState('All Tasks')
+  const [currentList, setCurrentList] = useState('Inbox')
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const intialTasks = await taskService.getAll()
+      setTasks(intialTasks)
+    }
+
+    getTasks()
+  }, [currentList])
+
   return (
     <>
       <CssBaseline enableColorScheme />
       <div className="homepage__container">
         <CategoryList setCurrentList={setCurrentList} />
-        <TaskList listName={currentList} />
+        <TaskList listName={currentList} tasks={tasks} updateTasks={setTasks} />
       </div>
     </>
   )
